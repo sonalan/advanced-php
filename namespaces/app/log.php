b1703546546a871bf6947c948f521735f7dcedac
sonalan <sinan@onalan.net>
2021-01-03 17:02:41 -0800

    added log classes in different namespaces

---
+++ namespaces/app/log.php
@@ -0,0 +1,9 @@
+<?php
+namespace App;
+
+class Log{
+
+    public static function setLog($log){
+        echo $log."\n";
+    }
+}
---
+++ namespaces/db/log.php
@@ -0,0 +1,8 @@
+<?php
+namespace Db;
+class Log{
+
+    public static function setLog($log){
+        echo $log."\n";
+    }
+}
