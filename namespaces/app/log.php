1df8385a40932735cbb35718d6a19b41ec7f5982
sonalan <sinan@onalan.net>
2021-01-03 17:25:23 -0800

    added class and method comments

--- namespaces/app/log.php
+++ namespaces/app/log.php
@@ -1,8 +1,15 @@
 <?php
 namespace App;
 
+/**
+ * Class Log
+ * @package App
+ */
 class Log{
 
+    /**
+     * @param $log
+     */
     public static function setLog($log){
         echo $log."\n";
     }
--- namespaces/db/log.php
+++ namespaces/db/log.php
@@ -1,7 +1,14 @@
 <?php
 namespace Db;
+/**
+ * Class Log
+ * @package Db
+ */
 class Log{
 
+    /**
+     * @param $log
+     */
     public static function setLog($log){
         echo $log."\n";
     }
